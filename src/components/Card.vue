5898f1d5dd0e24a848d8fbb774a1d1156e9ffe4e
lancamonica <mo_silva1015@icloud.com>
2021-10-15 10:37:36 -0300

    feat: creating posts screen, the card component and adjusting the menu

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card text-center q-ma-md q-pa-md shadow-up-3">
+    <slot class="card__image" name="image" />
+    <slot name="text" />
+    <hr />
+    <div class="card__data row inline content-start">
+      <slot class="card-data" name="data" />
+    </div>
+    <slot name="button" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss">
+  .card {
+    max-width: 300px;
+    max-height: 74vh;
+
+    &__image {
+      width: 100%;
+      height: 25vh;
+    }
+
+     &__data {
+      color: #4a4a4a;
+    }
+
+    hr {
+      border-top: 1px solid #efefef;
+    }
+
+    .sub-title {
+      color: #4a4a4a;
+    }
+
+  }
+</style>
