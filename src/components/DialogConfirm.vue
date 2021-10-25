2d0a50da831878eb4772453ac4f5ec7d4355e260
lancamonica <mo_silva1015@icloud.com>
2021-10-25 10:59:29 -0300

    feat: creating CRUD from authors

---
+++ src/components/DialogConfirm.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-dialog v-bind="$attrs" v-on="$listeners" persistent>
+    <q-card>
+      <q-card-section class="row items-center">
+        <q-avatar icon="delete_forever" color="grey-9" text-color="white" />
+        <span class="q-ml-sm">{{ content.subtitle }}</span>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat :label="content.labelCancel" color="brand" @click="cancel" />
+        <q-btn flat :label="content.labelSave" color="brand" @click="confirm" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  methods: {
+    confirm () {
+      this.$emit('confirm')
+    },
+    cancel () {
+      this.$emit('cancel')
+    }
+  }
+}
+</script>
---
+++ src/store/modules/authors.js
@@ -0,0 +1,103 @@
+import axios from 'axios'
+
+const state = {
+  authors: [],
+  detailsAuthor: {},
+  updateAuthor: {},
+  deleteAuthor: {}
+}
+
+const getters = {
+  authorsList: state => state.authors,
+  detailsAuthor: state => state.detailsAuthor,
+  putEditAuthor: state => state.updateAuthor,
+  authorDelete: state => state.deleteAuthor
+}
+
+const mutations = {
+  setAuthors (state, data) {
+    state.authors = data
+  },
+
+  setDetailsAuthor (state, data) {
+    state.detailsAuthor = data
+  },
+
+  setPutAuthor (state, data) {
+    state.updateAuthor = data
+  },
+
+  setDeleteAuthor (state, data) {
+    state.deleteAuthor = data
+  }
+}
+
+const actions = {
+  async getAuthors ({ commit }, params) {
+    try {
+      const { data } = await axios({
+        method: 'GET',
+        url: 'authors',
+        params
+      })
+
+      commit('setAuthors', data)
+    } catch {}
+  },
+
+  async getAuthor ({ commit }, id) {
+    try {
+      const { data } = await axios({
+        method: 'GET',
+        url: `authors/${id}`
+      })
+
+      commit('setDetailsAuthor', data)
+    } catch {}
+  },
+
+  async updateAuthor ({ commit }, content) {
+    try {
+      const { data } = await axios({
+        method: 'PUT',
+        url: `authors/${content.id}`,
+        data: content
+      })
+
+      commit('setPutAuthor', data)
+    } catch {}
+  },
+
+  async addAuthor (context, content) {
+    try {
+      const { data } = await axios({
+        method: 'POST',
+        url: 'authors',
+        data: content
+      })
+
+      return data
+    } catch {}
+  },
+
+  async deleteAuthor ({ commit }, id) {
+    console.log(id)
+    try {
+      const { data } = await axios({
+        method: 'DELETE',
+        url: `authors/${id}`
+      })
+
+      commit('setDeleteAuthor', data)
+    } catch {}
+  }
+}
+
+export default {
+  namespaced: true,
+
+  state,
+  getters,
+  mutations,
+  actions
+}
